eff4647446d9a7702c667194a595403f6cfc6c51
Anna <ponchucha@yandex.ru>
2022-12-19 13:31:35 +0300

    add complete homework

---
+++ task01/Program.cs
@@ -0,0 +1,68 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3 */
+
+// метод получения массива чисел на вход
+int[] GetArrayFromConsole(string message)
+{
+    Console.WriteLine("Введите числа через запятую или пробел");
+
+    string inputString = Console.ReadLine() ?? "";
+    char[] charSeparator = { ' ', ',' }; // здесь указаны разделители, которые будут использоваться в методе Split
+    string[] arrayStr = inputString.Split(charSeparator, StringSplitOptions.RemoveEmptyEntries); // Split - преобразует текст в массив строк. 
+    // StringSplitOptions.RemoveEmptyEntries - необязательный параметр, убирающий пустые элементы
+
+    int[] array = new int[arrayStr.Length]; // создаём массив натуральных чисел, куда будем складывать значения
+    for (int i = 0; i < arrayStr.Length; i++)
+    {
+        int number;
+        while (true)
+        {
+            if (int.TryParse(arrayStr[i], out number))
+            {
+                array[i] = number;
+                break;
+            }
+            else
+            {
+                Console.WriteLine($"{arrayStr[i]} - не число. Даже на него не похоже. Введите нормальное число взамен.");
+                arrayStr[i] = Console.ReadLine() ?? "";
+            }
+        }
+    }
+    return array;
+}
+
+// метод печати массива
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}]");
+    Console.WriteLine();
+}
+
+// метод подсчёта количества положительных элементов
+
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
+
+int[] array = GetArrayFromConsole("Введите элементы массива");
+Console.Write($"Задан массив из {array.Length} чисел: ");
+PrintArray(array);
+Console.WriteLine($"Количество положительных элементов массива равно {CountPositiveNumbers(array)}");
+
---
+++ task02/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+// метод получения числа на вход
+double GetNumberFromConsole (string message)
+{
+    Console.WriteLine(message);
+    double number;
+
+    while (true)
+    {
+        if (double.TryParse(Console.ReadLine(), out number))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Гениально! Но нам нужно не это, нам нужно число.");
+        }
+    }
+    return number;
+}
+
+// Метод нахождения точки пересечения прямых
+(double, double) GetCrossLocation (double b1, double k1, double b2, double k2)
+{
+    double y;
+    double x;
+
+    // (k1 * x) + b1 - (k2 * x) - b2 = 0;
+    // k1 * x - k2 * x = b2 - b1;
+    // (k1 - k2) * x = b2 - b1;
+
+    x = (b2 - b1) / (k1 - k2);
+    y = k1 * x + b1;
+
+    return (x, y);
+}
+
+Console.WriteLine("Даны две прямые, заданные уравнениями: y = k1 * x + b1, y = k2 * x + b2");
+double b1 = GetNumberFromConsole("Введите число b1");
+double k1 = GetNumberFromConsole("Введите число k1");
+double b2 = GetNumberFromConsole("Введите число b2");
+double k2 = GetNumberFromConsole("Введите число k2");
+(double x, double y) = GetCrossLocation(b1, k1, b2, k2);
+
+Console.WriteLine ($"Точка пересечения двух прямых имеет координаты [{x},{y}]");
+
+
